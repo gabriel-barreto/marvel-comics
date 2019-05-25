c265a52e794e3b118e2aeec9aca3522aea2faa2a
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-05-25 19:08:18 -0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Reactotron from "reactotron-react-js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        setTimeout(() => {
+            Reactotron.log(process.env);
+            Reactotron.log(process.env.REACT_APP_MARVEL_ACCESS_KEY);
+            Reactotron.log(process.env.REACT_APP_MARVEL_SECRET_KEY);
+        }, 3000);
+    }, []);
+
+    return <div className="App">Hello World!</div>;
 }
 
 export default App;
