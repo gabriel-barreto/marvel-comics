20ad7f98672795bb882731cba78966bcecace5d7
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-06-08 23:54:22 -0300

    Add title to grid

--- src/Containers/Home/grid.js
+++ src/Containers/Home/grid.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./grid.css";
 
-const Grid = ({ children }) => <div className="wrapper grid">{children}</div>;
+const Grid = ({ year, children }) =>
+    <section className="gallery">
+        <h2 className="title">{year}'s Comics</h2>
+        <div className="wrapper grid">{children}</div>;
+    </section>
 
 export default Grid;
